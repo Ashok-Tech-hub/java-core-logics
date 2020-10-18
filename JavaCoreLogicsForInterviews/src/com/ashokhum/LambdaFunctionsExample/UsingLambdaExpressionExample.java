88da00bab57f80063a93c5775ea571c22ee3425d
Devidkumar <Devidkumar@Devidkumar-PC>
2020-10-18 19:32:27 +0530

    updated lambda functions

---
+++ JavaCoreLogicsForInterviews/src/com/ashokhum/LambdaFunctionsExample/UsingLambdaExpressionExample.java
@@ -0,0 +1,19 @@
+package com.ashokhum.LambdaFunctionsExample;
+
+
+@FunctionalInterface  //It is optional  
+interface Drawable1{  
+    public void draw();  
+}  
+  
+public class UsingLambdaExpressionExample {  
+    public static void main(String[] args) {  
+        int width=10;  
+          
+        //with lambda  
+        Drawable1 d2=()->{  
+            System.out.println("Drawing "+width);  
+        };  
+        d2.draw();  
+    }  
+}  
